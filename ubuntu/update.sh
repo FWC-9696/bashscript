549b371b46c7d67546538e317c5c1e71f2409acd
FWC-9696 <jrichter96@hotmail.com>
2023-03-29 17:47:24 -0400

    Fix Kernel Detection

--- ubuntu/update.sh
+++ ubuntu/update.sh
@@ -6,7 +6,6 @@ do
 DATE=`date '+%F_%H:%M:%S'`
 sudo apt update && sudo apt upgrade -y
 ../version.sh
-echo ""
 echo "***UPDATED ON $DATE***"
 echo ""
 echo ""
--- version.sh
+++ version.sh
@@ -3,7 +3,8 @@ ID=`lsb_release -i | cut -f2`
 RELEASE=`lsb_release -r | cut -f2`
 CODENAME=`lsb_release -c | cut -f2`
 FULLNAME="$ID $RELEASE "$CODENAME""
-KERNEL=`cat /proc/version_signature`
+KERNEL=`uname -srm`
 echo ""
 echo -e "Kernel: \t $KERNEL"
 echo -e "Release: \t $FULLNAME"
+echo ""
