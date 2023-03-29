3a6aed8762235948bd66ccf3aca759d6e6e45623
FWC-9696 <jrichter96@hotmail.com>
2023-03-29 16:20:14 -0400

    Add Version Readout

--- ubuntu/update.sh
+++ ubuntu/update.sh
@@ -3,11 +3,18 @@ while :
 do 
 DATE=`date '+%F_%H:%M:%S'`
 sudo apt update && sudo apt upgrade -y
+ID=`lsb_release -i | cut -f2`
+RELEASE=`lsb_release -r | cut -f2`
+CODENAME=`lsb_release -c | cut -f2`
+FULLNAME="$ID $RELEASE "$CODENAME""
+KERNEL=`cat /proc/version_signature`
 echo ""
-uname -r
+echo -e "Kernel: \t $KERNEL"
+echo -e "Release: \t $FULLNAME"
 echo ""
 echo "***UPDATED ON $DATE***"
 echo ""
 echo ""
+echo "********************************************************************************"
 sleep 3600
 done
