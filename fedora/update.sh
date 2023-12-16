4111e581ea5304285cb5e7f3937bbc82666df59e
FWC-9696 <jrichter96@hotmail.com>
2023-12-15 19:46:18 -0500

    Add ani-cli update

--- fedora/update.sh
+++ fedora/update.sh
@@ -2,8 +2,10 @@
 SCRIPTDIR="$(dirname $(readlink -f $0))"
 DATE=`date '+%F_%H:%M:%S'`
 sudo dnf update -y
-$SCRIPTDIR/../version.sh
 echo ""
+echo "ani-cli: "
+sudo ani-cli -U
+$SCRIPTDIR/../version.sh
 sudo needs-restarting -r
 echo ""
 echo "***UPDATED ON $DATE***"
