319cc53b58d4550d8257dafe4180e356ba795358
FWC-9696 <jrichter96@hotmail.com>
2023-12-20 17:28:15 -0500

    add -y flag

--- fedora/system-upgrade.sh
+++ fedora/system-upgrade.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPTDIR="$(dirname $(readlink -f $0))"
 $SCRIPTDIR/update.sh
-sudo dnf install gnome-software
+sudo dnf install gnome-software -y
 sudo dnf upgrade --refresh
 gnome-software --autoupdate --mode=updates
