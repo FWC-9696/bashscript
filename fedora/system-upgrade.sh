3db678f24c28c967eb2a84523716403d1685a0f9
FWC-9696 <jrichter96@hotmail.com>
2023-12-20 21:16:28 -0500

    polish system-upgrade w/ version # loop

--- fedora/system-upgrade.sh
+++ fedora/system-upgrade.sh
@@ -6,22 +6,37 @@ sudo dnf upgrade --refresh
 #(gnome-software --autoupdate --mode=updates &)
 echo ""
 echo "WARNING: IF NOT DONE YET, YOU MUST REBOOT THE SYSTEM NOW"
-echo "Continue without reboot? [y/n]"
+echo "Continue WITHOUT Rebooting? [y/n]"
 read continue
 if [ "$continue" != "y" ]; then
     exit
 fi
+echo ""
 echo "Your Current Fedora Version is:"
 $SCRIPTDIR/../version.sh
-echo "Which Fedora Version to Upgrade to? (Input a Number or 'rawhide')"
-read new_version
+while :
+do
+	echo "Which Fedora Version to Upgrade to? (Input a Number or 'rawhide')"
+	read new_version
+	echo ""
+	echo "Install Fedora $new_version? [y/n]"
+	read continue
+		if [ "$continue" = "y" ]; then
+			break
+		fi
 echo ""
-echo "You input $new_version. Is that correct? [y/n]"
-read continue
-if [ "$continue" != "y" ]; then
-    exit
-fi
+done
 echo ""
-echo "Installing Fedora $new_version"
-sudo dnf system-upgrade download --releasever=$new_version --allowerasing
-sudo dnf system-upgrade reboot
+echo "Installing Fedora $new_version..."
+echo ""
+echo "The GPG Key Webpage Will Now Open." 
+echo "Keep this open for later."
+echo ""
+sleep 3
+xdg-open https://getfedora.org/security
+echo ""
+echo 'Note: "dnf upgrade --refresh" has alredy been run!'
+#sudo dnf system-upgrade download --releasever=$new_version --allowerasing
+echo ""
+echo 'Rebooting to install the upgrade...'
+#sudo dnf system-upgrade reboot
