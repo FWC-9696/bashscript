90a0d485c017b9034b502d17199545d6192ae21a
FWC-9696 <jrichter96@hotmail.com>
2023-12-20 20:05:12 -0500

    finish system-upgrade.sh

--- fedora/system-upgrade.sh
+++ fedora/system-upgrade.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 SCRIPTDIR="$(dirname $(readlink -f $0))"
 $SCRIPTDIR/update.sh
-sudo dnf install gnome-software -y
+sudo dnf install gnome-software dnf-utils -y
 sudo dnf upgrade --refresh
-gnome-software --autoupdate --mode=updates
+#(gnome-software --autoupdate --mode=updates &)
+echo ""
+echo "WARNING: IF NOT DONE YET, YOU MUST REBOOT THE SYSTEM NOW"
+echo "Continue without reboot? [y/n]"
+read continue
+if [ "$continue" != "y" ]; then
+    exit
+fi
+echo "Your Current Fedora Version is:"
+$SCRIPTDIR/../version.sh
+echo "Which Fedora Version to Upgrade to? (Input a Number or 'rawhide')"
+read new_version
+echo ""
+echo "You input $new_version. Is that correct? [y/n]"
+read continue
+if [ "$continue" != "y" ]; then
+    exit
+fi
+echo ""
+echo "Installing Fedora $new_version"
+sudo dnf system-upgrade download --releasever=$new_version --allowerasing
+sudo dnf system-upgrade reboot
