9d5a16a7526ff987bf9b38f268e4c4b35556f78a
FWC-9696 <jrichter96@hotmail.com>
2023-12-20 21:46:53 -0500

    Moved install files from LXQt Config

---
+++ fedora/install.sh
@@ -0,0 +1,3 @@
+sudo dnf install gnome-system-monitor gnome-weather gnome-screenshot gnome-software lsb_release blueman system-config-printer -y
+ln -s /bashscript/fedora/update.sh ../update
+source ~/.bashrc
