d6756881996539374c373ac48f8cf82239a2fb46
FWC-9696 <jrichter96@hotmail.com>
2023-12-25 13:37:19 -0500

    Add lsb_core to install packages for fedora

--- fedora/update.sh
+++ fedora/update.sh
@@ -3,6 +3,7 @@ SCRIPTDIR="$(dirname $(readlink -f $0))"
 DATE=`date '+%F_%H:%M:%S'`
 sudo dnf install dnf-utils -y
 sudo dnf install dnf-plugin-system-upgrade
+sudo dnf install redhat-lsb-core
 sudo dnf upgrade -y
 echo ""
 echo "ani-cli: "
