289075b48bad71a5b8a938c165d02e69efbb69da
FWC-9696 <jrichter96@hotmail.com>
2023-12-29 22:21:14 -0500

    Add spaces to update script

--- fedora/update.sh
+++ fedora/update.sh
@@ -2,9 +2,13 @@
 SCRIPTDIR="$(dirname $(readlink -f $0))"
 DATE=`date '+%F_%H:%M:%S'`
 sudo dnf install dnf-utils -y
+echo ""
 sudo dnf install dnf-plugin-system-upgrade -y
+echo ""
 sudo dnf install redhat-lsb-core -y --allowerasing
+echo ""
 sudo dnf upgrade -y
+echo ""
 sudo flatpak update -y
 echo ""
 echo "ani-cli: "
