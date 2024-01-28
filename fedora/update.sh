dc9c1289ae4c980879e27eed6af5e79963ce5be7
FWC-9696 <jrichter96@hotmail.com>
2024-01-28 15:16:59 -0500

    move date inside of loop

--- fedora/update.sh
+++ fedora/update.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 SCRIPTDIR="$(dirname $(readlink -f $0))"
-DATE=`date '+%F_%H:%M:%S'`
 sudo dnf install dnf-utils -y
 echo ""
 sudo dnf install dnf-plugin-system-upgrade -y
 echo ""
 sudo dnf install redhat-lsb-core -y --allowerasing
 echo ""
+while :
+do
+DATE=`date '+%F_%H:%M:%S'`
 sudo dnf upgrade -y
 echo ""
 sudo flatpak update -y
@@ -19,3 +21,5 @@ echo ""
 echo "***UPDATED ON $DATE***"
 echo ""
 echo "********************************************************************************"
+sleep 3600
+done
