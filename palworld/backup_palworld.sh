8626393fcbf26b20946e98164c45cf288932807d
FWC-9696 <jrichter96@hotmail.com>
2024-02-21 12:43:45 -0500

    Remove DB Dump for Pleroma. Add Palworld Backup

---
+++ palworld/backup_palworld.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp -r /home/james/.local/share/Steam/steamapps/common/PalServer/Pal/Saved/SaveGames /backup/palworld
+cp /home/james/.local/share/Steam/steamapps/common/PalServer/Pal/Saved/Config/LinuxServer/PalWorldSettings.ini /backup/palworld
+cp /home/james/.local/share/Steam/steamapps/common/PalServer/Pal/Saved/Config/LinuxServer/GameUserSettings.ini /backup/palworld
