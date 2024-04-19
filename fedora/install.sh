0f7c057c6dff0ab062212368b4ba6ff6a9cec305
FWC-9696 <jrichter96@hotmail.com>
2024-04-19 11:27:37 -0400

    add qt5ct to install list

--- fedora/install.sh
+++ fedora/install.sh
@@ -1,4 +1,4 @@
-sudo dnf install gnome-system-monitor gnome-weather gnome-screenshot gnome-software lsb_release blueman system-config-printer -y
+sudo dnf install gnome-system-monitor gnome-weather gnome-screenshot gnome-software lsb_release blueman system-config-printer qt5ct -y
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 sudo flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
 sudo flatpak install  chat.schildi.desktop
