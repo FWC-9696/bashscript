95236f762807816b1ebb1946bc6daae5cf4634be
FWC-9696 <jrichter96@hotmail.com>
2024-05-12 11:06:51 -0400

    Firefox Nightly 1-click Update + Run Added

--- firefox/update_firefox_nightly_tar.sh
+++ firefox/update_firefox_nightly_tar.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-sudo dnf install wget
 sudo wget -O /opt/FirefoxSetup.tar.bz2 "https://download.mozilla.org/?product=firefox-nightly-latest-ssl&os=linux64&lang=en-US"
 wait
 sudo tar xjf /opt/FirefoxSetup.tar.bz2 -C /opt
-sudo ln -s /opt/firefox/firefox /usr/local/bin/firefox
-sudo cp firefox-nightly.desktop /usr/local/share/applications
+firefox
