5179b1628bd617ce90ccd05b8245296fbcf14f1a
FWC-9696 <jrichter96@hotmail.com>
2024-05-11 22:53:42 -0400

    moved the install scripts to appropriate locations

--- ubuntu/install_firefox_flathub.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt install flatpak
-sudo apt install gnome-software-plugin-flatpak
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub org.mozilla.firefox
