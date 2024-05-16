856d529e07f8bf56bf5b83a8d0abb847fa256985
FWC-9696 <jrichter96@hotmail.com>
2024-05-15 23:48:19 -0400

    Added media to install scripts to make videos work in non-flatpak browsers

--- fedora/install.sh
+++ fedora/install.sh
@@ -1,4 +1,11 @@
+#Basic Gnome Apps
 sudo dnf install gnome-system-monitor gnome-weather gnome-screenshot gnome-software blueman system-config-printer qt5ct -y --allowerasing
+#Multimedia Stuff to make videos play
+sudo dnf group install multimedia 
+sudo dnf install \
+  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install ffmpeg-libs -y --allowerasing
+#Flathub Apps to Use
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 sudo flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
 #sudo flatpak install flathub chat.schildi.desktop -y
