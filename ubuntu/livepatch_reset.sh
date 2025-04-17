8a29b6823bf8ff6274937c92f50471f1c3ba8a78
FWC-9696 <jrichter96@hotmail.com>
2025-04-16 23:45:06 -0400

    added notes; added show-livepatch command for livepatch reset

--- ubuntu/livepatch_reset.sh
+++ ubuntu/livepatch_reset.sh
@@ -3,3 +3,4 @@ sudo pro disable livepatch
 sudo snap remove canonical-livepatch
 sudo rm -rf /snap/canonical-livepatch
 sudo pro enable livepatch
+gsettings set com.ubuntu.update-notifier show-livepatch-status-icon true
